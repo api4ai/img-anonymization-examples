d27935fef05ad4dceeb00b9abf792b0c9af11a80
Vladislav Gubarev <gubarev@api4.ai>
2022-07-08 15:36:30 +0300

    Python: Fixed flake8 warnings.

--- python/aiohttp/sample.py
+++ python/aiohttp/sample.py
@@ -32,7 +32,7 @@ OPTIONS = {
         'headers': {'A4A-CLIENT-APP-ID': 'sample'}
     },
     'rapidapi': {
-        'url': f'https://image-anonymization.p.rapidapi.com/v1/results',
+        'url': 'https://image-anonymization.p.rapidapi.com/v1/results',
         'headers': {'X-RapidAPI-Key': RAPIDAPI_KEY}
     }
 }
@@ -62,7 +62,7 @@ async def main():
         with open(path_to_image, 'wb') as img:
             img.write(base64.decodebytes(img_b64))
 
-        objects_pretty = json.dumps(resp_json["results"][0]["entities"][1]["objects"], indent=2)
+        objects_pretty = json.dumps(resp_json['results'][0]['entities'][1]['objects'], indent=2)
         print(f'💬 Hidden objects:\n{objects_pretty}\n')
 
         print(f'💬 The "result.{image_format}" image is saved to the current directory.')

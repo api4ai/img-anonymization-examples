231c479f2c46277b5932e8c08e5f1ad7cad95729
Vladislav Gubarev <gubarev@api4.ai>
2022-07-08 15:32:13 +0300

    Added python/aiohttp sample.

---
+++ python/aiohttp/sample.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+
+"""Example of using API4AI image anonymization API."""
+
+import asyncio
+import base64
+import json
+import os
+import sys
+
+import aiohttp
+
+
+# Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
+# For more details visit:
+#   https://api4.ai
+#
+# Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
+# For more details visit:
+#   https://rapidapi.com/api4ai-api4ai-default/api/image-anonymization/details
+MODE = 'demo'
+
+
+# Your RapidAPI key. Fill this variable with the proper value if you want
+# to try api4ai via RapidAPI marketplace.
+RAPIDAPI_KEY = ''
+
+
+OPTIONS = {
+    'demo': {
+        'url': 'https://demo.api4ai.cloud/img-anonymization/v1/results',
+        'headers': {'A4A-CLIENT-APP-ID': 'sample'}
+    },
+    'rapidapi': {
+        'url': f'https://image-anonymization.p.rapidapi.com/v1/results',
+        'headers': {'X-RapidAPI-Key': RAPIDAPI_KEY}
+    }
+}
+
+
+async def main():
+    """Entry point."""
+    image = sys.argv[1] if len(sys.argv) > 1 else 'https://storage.googleapis.com/api4ai-static/samples/img-anonymization-0.jpg'
+
+    async with aiohttp.ClientSession() as session:
+        if '://' in image:
+            # Data from image URL.
+            data = {'url': image}
+        else:
+            # Data from local image file.
+            data = {'image': open(image, 'rb')}
+        # Make request.
+        async with session.post(OPTIONS[MODE]['url'],
+                                data=data,
+                                headers=OPTIONS[MODE]['headers']) as response:
+            resp_json = await response.json()
+
+        image_format = resp_json['results'][0]['entities'][0]['format'].lower()
+        img_b64 = resp_json['results'][0]['entities'][0]['image'].encode('utf8')
+
+        path_to_image = os.path.join(f'result.{image_format}')
+        with open(path_to_image, 'wb') as img:
+            img.write(base64.decodebytes(img_b64))
+
+        objects_pretty = json.dumps(resp_json["results"][0]["entities"][1]["objects"], indent=2)
+        print(f'💬 Hidden objects:\n{objects_pretty}\n')
+
+        print(f'💬 The "result.{image_format}" image is saved to the current directory.')
+
+
+if __name__ == '__main__':
+    # Run async function in asyncio loop.
+    asyncio.run(main())

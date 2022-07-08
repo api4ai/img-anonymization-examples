4723b2e8159ea4a346531220041cd63e35b27755
Vladislav Gubarev <gubarev@api4.ai>
2022-07-08 15:46:27 +0300

    Added php/curl sample.

---
+++ php/curl/index.php
@@ -0,0 +1,68 @@
+#!/usr/bin/env php
+
+<?php
+// Example of using API4AI image anonymization API.
+
+// Use 'demo' mode just to try api4ai for free. Free demo is rate limited.
+// For more details visit:
+//   https://api4.ai
+
+// Use 'rapidapi' if you want to try api4ai via RapidAPI marketplace.
+// For more details visit:
+//   https://rapidapi.com/api4ai-api4ai-default/api/image-anonymization/details
+$MODE = 'demo';
+
+// Your RapidAPI key. Fill this variable with the proper value if you want
+// to try api4ai via RapidAPI marketplace.
+$RAPIDAPI_KEY = null;
+
+$OPTIONS = [
+    'demo' => [
+        'url' => 'https://demo.api4ai.cloud/img-anonymization/v1/results',
+        'headers' => ['A4A-CLIENT-APP-ID: sample']
+    ],
+    'rapidapi' => [
+        'url' => "https://image-anonymization.p.rapidapi.com/v1/results",
+        'headers' => ["X-RapidAPI-Key: {$RAPIDAPI_KEY}"]
+    ]
+];
+
+// Initialize request session.
+$request = curl_init();
+
+// Check if path to local image provided.
+$data = ['url' => 'https://storage.googleapis.com/api4ai-static/samples/img-anonymization-0.jpg'];
+if (array_key_exists(1, $argv)) {
+    if (strpos($argv[1], '://')) {
+        $data = ['url' => $argv[1]];
+    } else {
+        $filename = pathinfo($argv[1])['filename'];
+        $data = ['image' => new CURLFile($argv[1], null, $filename)];
+    }
+}
+
+// Set request options.
+curl_setopt($request, CURLOPT_URL, $OPTIONS[$MODE]['url']);
+curl_setopt($request, CURLOPT_HTTPHEADER, $OPTIONS[$MODE]['headers']);
+curl_setopt($request, CURLOPT_POST, true);
+curl_setopt($request, CURLOPT_POSTFIELDS, $data);
+curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+
+// Execute request.
+$result = curl_exec($request);
+
+// Decode response.
+$raw_response = json_decode($result, true);
+
+// Get image data from response.
+$img_base_64_str = $raw_response['results'][0]['entities'][0]['image'];
+$img_format = strtolower($raw_response['results'][0]['entities'][0]['format']);
+
+// Write image data to the file.
+file_put_contents("result.{$img_format}", base64_decode($img_base_64_str));
+
+echo "💬 Hidden objects:";
+echo json_encode($raw_response['results'][0]['entities'][1]['objects'], JSON_PRETTY_PRINT);
+
+echo "\n", "💬 The 'result.{$img_format}' image is saved to the current directory.", "\n";
+?>
